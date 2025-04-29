1dc4cdc63b039335a33ce0b9a17d4ce096210e13
Pinar <wi23b133@technikum-wien.at>
2025-04-30 00:01:05 +0200

    add changes invoice

---
+++ Frontend/js/myorders.js
@@ -0,0 +1,24 @@
+function loadOrders() {
+    fetch("../../backend/logic/getOrders.php")
+      .then(res => res.json())
+      .then(data => {
+        const container = document.getElementById("orders");
+        container.innerHTML = "";
+  
+        data.forEach(order => {
+          container.innerHTML += `
+            <div>
+              <h3>Bestellung #${order.id}</h3>
+              <p>Datum: ${order.created_at} – Gesamt: ${order.total_price} €</p>
+              <button onclick="showOrderDetails(${order.id})">Details</button>
+            </div>
+          `;
+        });
+      });
+  }
+  
+  function showOrderDetails(orderId) {
+    // TODO: hier kannst du fetch machen zu z.B. getOrderItems.php?orderId=...
+    alert("Details für Bestellung " + orderId + " werden später angezeigt.");
+  }
+  
